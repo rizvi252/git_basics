0d117917bccfa673589c2f63d5e34b4f14ebba8e
Imran Syed <rizvi252@gmail.com>
2026-02-13 15:58:29 -0500

    Add multiply(a, b) function and demo

--- calc.py
+++ calc.py
@@ -5,6 +5,13 @@ def add(a, b):
     return a + b
 
 
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
-    result = add(2, 3)
-    print(f"2 + 3 = {result}")
+    sum_result = add(2, 3)
+    product_result = multiply(2, 3)
+
+    print(f"2 + 3 = {sum_result}")
+    print(f"2 * 3 = {product_result}")
